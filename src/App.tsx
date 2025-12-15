56d48f089286286deb1e7037e2e95aacbebb70c4
Maarten Coppens <maarcopp@student.arteveldehs.be>
2025-12-15 11:57:04 +0100

    Initial Commit - Hogwarts 3D map Website

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex justify-center items-center">
+        <img src="/images/HP_Logo.svg" alt="Harry Potter Logo" />
+        <h1 className="text-4xl font-bold magical-glow">Explore Hogwarts</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
