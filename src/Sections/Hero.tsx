4f36882b8a3fdd63917d43add42ab2ea6e0fba30
Maarten Coppens <maarcopp@student.arteveldehs.be>
2025-12-15 17:29:46 +0100

    Particle background added

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -21,21 +21,27 @@ const Hero = () => {
   }, []);
 
   return (
-    <div
-      ref={sectionRef}
-      className="min-h-screen flex justify-center items-center flex-col gap-8 w-1/4 mx-auto text-center"
-    >
-      <img src="/images/HP_Logo.png" alt="Harry Potter Logo" className="w-30" />
-      <h1 className="text-4xl font-bold magical-glow hero-title">
-        Explore Hogwarts
-      </h1>
-      <p className="text-lg text-amber-50/90">
-        Step into an immersive 3D journey through the most magical castle in the
-        Wizarding World. Discover hidden chambers, legendary halls, and
-        enchanted grounds like never before.
-      </p>
-      <Button route="/map" label="Enter the Castle" />
-      <ScrollHint />
+    <div className="">
+      <div
+        ref={sectionRef}
+        className="min-h-screen flex justify-center items-center flex-col gap-8 w-1/4 mx-auto text-center"
+      >
+        <img
+          src="/images/HP_Logo.png"
+          alt="Harry Potter Logo"
+          className="w-30"
+        />
+        <h1 className="text-4xl font-bold magical-glow hero-title">
+          Explore Hogwarts
+        </h1>
+        <p className="text-lg text-amber-50/90">
+          Step into an immersive 3D journey through the most magical castle in
+          the Wizarding World. Discover hidden chambers, legendary halls, and
+          enchanted grounds like never before.
+        </p>
+        <Button route="/map" label="Enter the Castle" />
+        <ScrollHint />
+      </div>
     </div>
   );
 };
